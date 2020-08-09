28a38db9862193406e8722e4e27da5f07c108a12
Yingzhi <zhangyingzhianja@gmail.com>
2020-08-09 19:32:23 +0200

    Update test_image.py

--- test_image.py
+++ test_image.py
@@ -77,10 +77,11 @@ if __name__ == '__main__':
     class_info, color_info = ade20k.init_ade20k_class_color_info(Path('/home/hchen/Documents/yzh/swiftnet2/swiftnet/datasets'))
     #print(class_info, color_info)
     
-    store_dir =  f'{dir_path}/out/'
+    store_dir =  f'test_image/'
     to_color = ColorizeLabels(color_info)
     to_image = Compose([DenormalizeTh(scale, mean, std), Numpy(), to_color])
-    #eval_observers = [StorePreds(store_dir, to_image, to_color)] #store colorized image
+    eval_observers = [StorePreds(store_dir, to_image, to_color)] #store colorized image
+    
     store_img = np.concatenate([i.astype(np.uint8) for i in [to_image['image'], self.to_color(pred)]], axis=0)
     store_img = pimg.fromarray(store_img)
     store_img.thumbnail((960, 1344))
